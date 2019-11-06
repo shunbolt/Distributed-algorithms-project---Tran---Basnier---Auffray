940c7b4280c38823c3002410fa6b53e22afa5f9b
Shunbolt <raphaeltrann@hotmail.com>
2019-11-06 09:45:33 +0100

    Start of project V1

---
+++ src/main/java/com/lightbend/akka/sample/Process.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lightbend.akka.sample;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedAbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Raphael Tran
+ */
+public class Process extends UntypedAbstractActor {
+    
+    // Logger attached to the process
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+    
+    // Actor reference that points to neighbor actor
+    
+    private ArrayList<ActorRef> ActorList;
+        
+    static public class ProcessList implements Serializable {
+		public final ArrayList<ActorRef> list;
+	
+		public ProcessList(ArrayList<ActorRef> l) {
+                        // this.list = org.apache.commons.lang3.SerializationUtils.clone(l);
+                        this.list = l;
+                }
+                
+                public ArrayList<ActorRef> getlist(){
+                    return this.list;
+                }
+    }   
+    
+    static public class Welcome{
+        public final String msg;
+        
+        public Welcome(String m){
+            this.msg = m;
+        }
+    }
+    
+    
+    // Empty constructor
+    public Process() {};
+    
+    
+    
+    // Static function that creates the actor
+    public static Props createActor() {
+		return Props.create(Process.class, () -> {
+			return new Process();
+		});
+	}
+    
+    
+    // createReceive function which describes the behavior of the process when it receives a message
+    @Override
+	public void onReceive(Object message) throws Throwable {
+                // Store the list of processes into the ProcessList class
+                if(message instanceof ProcessList){
+                    this.ActorList = ((ProcessList) message).getlist();
+                    log.info("[" + getSelf().path().name()+ "] has received the list of all actors in the system " ); 
+                    // Loop through all the processes to give a welcome message
+                    for(ActorRef d : this.ActorList ){
+                        Welcome w = new Welcome("Welcome process !");
+                        d.tell(w,getSelf());
+                    }
+                } 
+                // Behavior for a welcome message
+                if(message instanceof Welcome){
+                    Welcome w = (Welcome) message;
+                    log.info("["+getSelf().path().name()+"] received message from ["+ getSender().path().name() +"] with content: ["+w.msg+"]");
+                }
+	}
+    
+}
---
+++ src/main/java/com/lightbend/akka/sample/Process_system.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lightbend.akka.sample;
+
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import com.lightbend.akka.sample.Process.ProcessList;
+import com.lightbend.akka.sample.Process.Welcome;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Raphael Tran
+ */
+public class Process_system {
+    public static void main(String[] args) {
+        // Instantiate system actor
+            final ActorSystem system = ActorSystem.create("Process_Tran");
+        try {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("How many processes do you want in the system ?");
+            int n = reader.nextInt();
+            
+            ArrayList <ActorRef> listProcesses = new ArrayList<>();
+            
+            
+            // Instantiate n processes in a ArrayList
+            for(int i = 0; i < n; i++){
+                String pname = "process" + i;
+                listProcesses.add(system.actorOf(Process.createActor(), pname));
+            }
+            
+            // Loop and message the n processes with the list of processes
+            
+            for(ActorRef process : listProcesses ){
+                ProcessList pl;
+                pl = new ProcessList(listProcesses);
+                process.tell(pl, ActorRef.noSender());  
+            }
+            
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+            } catch (IOException ioe) {
+            } finally {
+                system.terminate();
+            }
+        
+    }
+}
