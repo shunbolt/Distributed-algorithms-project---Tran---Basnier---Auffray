9c6250c2baebab99076c4f877a6b3ff437fb325d
Shunbolt <raphaeltrann@hotmail.com>
2019-11-14 11:38:12 +0100

    Added class WriteAnswer (ACK after a write message)

---
+++ src/main/java/com/lightbend/akka/sample/WriteAnswer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lightbend.akka.sample;
+
+/**
+ *
+ * @author Raphael Tran
+ */
+public class WriteAnswer {
+    
+    private int seq;
+    
+            public WriteAnswer(int s) {
+                       
+                        this.seq = s;
+                }
+            
+            public int getSeq(){
+                return this.seq;
+            }  
+}
